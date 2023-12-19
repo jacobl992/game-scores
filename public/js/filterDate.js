030fbf5a984e9be8e65016dde5f3d75e05331732
jacobl992 <jake_lewis12@hotmail.co.uk>
2023-12-19 16:06:51 +0000

    added date display when date filtered

--- public/js/filterDate.js
+++ public/js/filterDate.js
@@ -1,6 +1,24 @@
 const dateSelect = document.querySelector('.date');
-const filterButton = document.querySelector('.filter-button');const unresponseiveTable = document.querySelector('#unresponsive-table');
+const filterButton = document.querySelector('.filter-button');
 const clearFilter = document.querySelector('.clear-filter');
+const dateOnLoad = document.querySelector('#selected-date');
+
+const initialDateParameter = () => {
+    const pathSegments = window.location.pathname.split('/');
+    const dateIndex = pathSegments.indexOf('scores') + 1;
+
+    if (dateIndex > 0 && dateIndex < pathSegments.length) {
+        return pathSegments[dateIndex];
+    }
+
+    return '';
+}
+
+const dateOnLoadHeader = () => {
+    dateOnLoad.innerHTML = initialDateParameter()
+}
+
+dateOnLoadHeader();
 
 clearFilter.addEventListener('click', e => {
     window.location.href = '../scores';
