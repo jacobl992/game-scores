11080eeb5f1d0a5dacd575bf7b3e2b871d83ac7b
jacobl992 <jake_lewis12@hotmail.co.uk>
2023-12-19 15:44:39 +0000

    added backend validation

--- src/Controllers/FilterDateController.php
+++ src/Controllers/FilterDateController.php
@@ -25,6 +25,16 @@ class FilterDateController extends Controller
     public function __invoke(Request $request, Response $response, array $args): Response
     {
         $date = $args['date'];
+        $validDatesReturned = $this->scoresModel->getUniqueDates();
+        $validDates = [];
+
+        foreach ($validDatesReturned as $validDateReturned) {
+            $validDates[] = $validDateReturned['date'];
+        }
+
+        if (!in_array($date, $validDates)) {
+            return $response->withStatus(400)->withJson(['Error' => $date . ' not in database']);
+        }
 
         $args['allScores'] = $this->scoresModel->getScores($date);
         $args['gameList'] = $this->gamesModel->getGameList();
