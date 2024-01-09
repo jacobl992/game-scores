81442ca33cef70ab33565fd0ef07e88ffb92d31c
jacobl992 <jake_lewis12@hotmail.co.uk>
2024-01-09 13:28:29 +0000

    remvoed console logs

--- public/js/winningColors.js
+++ public/js/winningColors.js
@@ -1,4 +1,3 @@
-
 const cathyTotalScore = document.querySelector('#cathy-row-totalscore');
 const jakeTotalScore = document.querySelector('#jake-row-totalscore');
 const cathyWinTally = document.querySelector('#cathy-row-wintally');
@@ -8,12 +7,6 @@ const cathyTotalScoreInt = parseInt(cathyTotalScore.dataset.totalscore, 10);
 const jakeTotalScoreInt = parseInt(cathyWinTally.dataset.wintally, 10);
 const cathyWinTallyInt = parseInt(jakeTotalScore.dataset.totalscore, 10);
 const jakeWinTallyInt = parseInt(jakeWinTally.dataset.wintally, 10);
-
-
-console.log(`Cathy's Total Score: ${cathyTotalScoreInt}`);
-console.log(`Jake's Total Score: ${jakeTotalScoreInt}`);
-console.log(`Cathy's Win Tally: ${cathyWinTallyInt}`);
-console.log(`Jake's Win Tally: ${jakeWinTallyInt}`);
 
 if (cathyTotalScoreInt > jakeTotalScoreInt) {
     jakeTotalScore.classList.add('winner');
