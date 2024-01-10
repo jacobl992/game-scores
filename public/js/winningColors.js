02703f93f86dd89a4f05008b79b459f3f1adc773
jacobl992 <jake_lewis12@hotmail.co.uk>
2024-01-10 10:35:36 +0000

    corrected js variables for wintally

--- public/js/winningColors.js
+++ public/js/winningColors.js
@@ -4,8 +4,8 @@ const cathyWinTally = document.querySelector('#cathy-row-wintally');
 const jakeWinTally = document.querySelector('#jake-row-wintally');
 
 const cathyTotalScoreInt = parseInt(cathyTotalScore.dataset.totalscore, 10);
-const jakeTotalScoreInt = parseInt(cathyWinTally.dataset.wintally, 10);
-const cathyWinTallyInt = parseInt(jakeTotalScore.dataset.totalscore, 10);
+const jakeTotalScoreInt = parseInt(jakeTotalScore.dataset.totalscore, 10);
+const cathyWinTallyInt = parseInt(cathyWinTally.dataset.wintally, 10);
 const jakeWinTallyInt = parseInt(jakeWinTally.dataset.wintally, 10);
 
 if (cathyTotalScoreInt > jakeTotalScoreInt) {
